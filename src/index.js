fd8684d6f00699d936ffcc73e6745dd184bd93e8
sidnei <sialkas@gmail.com>
2019-07-01 16:39:02 -0300

    Adicionado modulo EJS

--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ const request = require("request-promise-native");
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', './src/views');
+
 app.get('/produtos', async(req, res) => {
 	const result = await request.get('http://localhost:3000/api/v1/products');
-	res.send('Olá');
+	// console.log(JSON.parse(result).data);
+	// res.send('Olá');
+	res.render('products');
+
 });
 
 app.listen(3001, () => {
